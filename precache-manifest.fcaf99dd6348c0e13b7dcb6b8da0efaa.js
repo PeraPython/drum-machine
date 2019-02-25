c5f05215fdff78a26960551e4c66c9cc07060d73
Marko <marko.perendic@yahoo.com>
2019-02-25 14:36:57 +0100

    Updates

---
+++ precache-manifest.fcaf99dd6348c0e13b7dcb6b8da0efaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c73e5ac2cc3e61c10a29",
+    "url": "/drum-machine/static/js/runtime~main.c73e5ac2.js"
+  },
+  {
+    "revision": "993037eed4a40b670bc5",
+    "url": "/drum-machine/static/js/main.993037ee.chunk.js"
+  },
+  {
+    "revision": "bb1cfbc77e0591448a8c",
+    "url": "/drum-machine/static/js/2.bb1cfbc7.chunk.js"
+  },
+  {
+    "revision": "993037eed4a40b670bc5",
+    "url": "/drum-machine/static/css/main.6cb6f2aa.chunk.css"
+  },
+  {
+    "revision": "44b7047b61e95088f15f11228dd617af",
+    "url": "/drum-machine/index.html"
+  }
+];
